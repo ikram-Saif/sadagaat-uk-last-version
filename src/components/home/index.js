a0215f6e75f8dd5a6074e35054868ce5c17a33ef
smsm95 <osamamirghani95@gmail.com>
2020-04-13 01:02:03 +0200

    Sliders

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import Slider from '../../components/slider';
-import Hubs from '../../components/base_hubs';
-import About from '../../components/about';
-import ProjectSlider from '../../components/projects/projectsSlider'
-import Numbers from '../../components/numbers'
-import Photo from '../../components/photo_gallery'
-import Video from '../../components/video'
-import Events from '../../components/events'
-import Parteners from '../parteners';
-import Projects from '../projects';
-
+import React from "react";
+import Slider from "../../components/slider";
+import Hubs from "../../components/base_hubs";
+import About from "../../components/about";
+import ProjectSlider from "../../components/projects/projectsSlider";
+import ProjectCarousel from "../../components/projects/projectCarousel";
+import Numbers from "../../components/numbers";
+import Photo from "../../components/photo_gallery";
+import Video from "../../components/video";
+import Events from "../../components/events";
+import Parteners from "../parteners";
+import Projects from "../projects";
 
 function Home() {
-
-   return (
-   
-   <div className="main-content">
-
-     <Slider/>
-     <Hubs/>
-     <About/>
-     <ProjectSlider/>
-     <Numbers/>
-     <Photo/>
-     <Video/>
-     <Events/>
-     <Parteners/>   
-     </div>
-
-);
+  return (
+    <div className="main-content">
+      <Slider />
+      <Hubs />
+      <About />
+      <ProjectCarousel />
+      <Numbers />
+      <Photo />
+      <Video />
+      <Events />
+      <Parteners />
+    </div>
+  );
 }
 
 export default Home;
