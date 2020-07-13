679f5b0d8c437c797f48c8b1c0a673fa50208583
InasMahgoub <37179790+InasMahgoub@users.noreply.github.com>
2020-07-13 11:24:04 +0200

    news carousel

--- src/components/news/single-news.js
+++ src/components/news/single-news.js
@@ -7,19 +7,19 @@ import Header from "../sub_page_header";
 import SocialMedia from "../social media/social-media";
 import Carousel from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
-import testVideo from './ROI Calculator for UX Solutions.mp4'
-
+import testVideo from "./ROI Calculator for UX Solutions.mp4";
 
 class SinglNews extends Component {
   constructor() {
     super();
     this.state = {
       news: [],
-      newsImages : [],
-      newsVideos :[
-        {id:1,name :testVideo},{id:2,name : testVideo}
+      newsImages: [],
+      newsVideos: [
+        { id: 1, name: testVideo },
+        { id: 2, name: testVideo },
       ],
-      allMedia:[]
+      allMedia: [],
     };
   }
 
@@ -30,14 +30,14 @@ class SinglNews extends Component {
       const { data: news } = await axios.get(`${address()}news/${id}`, {
         headers: { "accept-language": `${i18n.language}` },
       });
-      this.setState({ 
+      this.setState({
         news,
-        newsImages:news.images,
-       });
+        newsImages: news.images,
+      });
     } catch (error) {
       console.log("can not load news for the home page slider");
     }
-    this.fillMediaArray()
+    this.fillMediaArray();
   }
   async componentWillReceiveProps() {
     let id = this.props.match.params.news_id;
@@ -51,54 +51,46 @@ class SinglNews extends Component {
       console.log("can not load news for the home page slider");
     }
   }
-   fillMediaArray =()=>{
-    const news_images = this.state.newsImages
-    const news_videos = this.state.newsVideos
-    const allMedia = []
+  fillMediaArray = () => {
+    const news_images = this.state.newsImages;
+    const news_videos = this.state.newsVideos;
+    const allMedia = [];
     /**fill array with default Image */
     allMedia.push({
-          type :'image',
-          id : this.state.news.id,
-          name : this.state.news.id
-    })
+      type: "image",
+      id: this.state.news.id,
+      name: this.state.news.id,
+    });
 
-    if(news_images.length > 0)
-    {
-      news_images.map((image) =>{
-
+    if (news_images.length > 0) {
+      news_images.map((image) => {
         allMedia.push({
-          type :'image',
-          id : image.id,
-          name : image.name
-        })
-      
-      })
+          type: "image",
+          id: image.id,
+          name: image.name,
+        });
+      });
     }
-    
-    if(news_videos.length > 0)
-    {
-    news_videos .map((video) =>{
 
-      allMedia.push({
-        type :'video',
-        id : video.id,
-        name : video.name
-      })
-    
-    })
-  }
-    this.setState({allMedia})
-    console.log(allMedia)
-
-   }
-
+    if (news_videos.length > 0) {
+      news_videos.map((video) => {
+        allMedia.push({
+          type: "video",
+          id: video.id,
+          name: video.name,
+        });
+      });
+    }
+    this.setState({ allMedia });
+    console.log(allMedia);
+  };
 
   render() {
     const { t } = this.props;
     const { news } = this.state;
-     //const news_images = this.state.newsImages
+    //const news_images = this.state.newsImages
     // const news_videos = this.state.newsVideos
-    const allMedia = this.state.allMedia
+    const allMedia = this.state.allMedia;
 
     // news_images .map((image) =>{
 
@@ -107,7 +99,7 @@ class SinglNews extends Component {
     //     id : image.id,
     //     name : image.name
     //   })
-    
+
     // })
     // news_videos .map((video) =>{
 
@@ -116,13 +108,9 @@ class SinglNews extends Component {
     //     id : video.id,
     //     name : video.name
     //   })
-    
+
     // })
     // console.log(allMedia)
-
-
-    
-
 
     return (
       <div>
@@ -131,19 +119,19 @@ class SinglNews extends Component {
         <section>
           <div className="container mt-30 mb-30 pt-30 pb-30">
             <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-7">
                 <div class="blog-posts single-post">
                   <article class="post clearfix mb-0">
                     <div class="entry-header">
-                     {allMedia.length > 1? 
-                     (<Carousel  
-                          slidesPerScroll={1}
+                      {allMedia.length > 1 ? (
+                        <Carousel
+                          // slidesPerScroll={1}
                           //autoPlay={6000}
                           rtl
                           arrowLeft={
                             <i
                               className="fa fa-chevron-right fa-2x"
-                              style={{ margin:"10px" }}
+                              style={{ margin: "10px" }}
                             />
                           }
                           arrowRight={
@@ -160,87 +148,93 @@ class SinglNews extends Component {
                           breakpoints={{
                             1000: {
                               // these props will be applied when screen width is less than 1000px
-                              slidesPerPage: 2,
+                              slidesPerPage: 1,
+                              slidesPerScroll: 1,
                               clickToChange: false,
-                              centered: false,
-          
+                              centered: true,
                               infinite: false,
                             },
                             500: {
                               slidesPerPage: 1,
                               slidesPerScroll: 1,
                               clickToChange: false,
-                              centered: false,
+                              centered: true,
                               animationSpeed: 2000,
                               infinite: false,
                             },
-                          }} >
-
-                  {allMedia.map((media) =>(
-                    media.type === 'image'?(
-                            <div
-                              class="post-thumb thumb"
-                              style={{ mxaHeight: "500px" }} >
-                              <img
-                                src={`${address()}news/${media.name}/image`}
-                                className="img-fullwidth img-responsive"
-                                alt=""
-                                style = {{height:'400px'}}
-                              />
-                            </div>
-                      ):
-                      (
-                         <video 
-                              class="post-thumb thumb"
-                              style={{ mxaHeight: "500px" }} 
-                              controls 
-                              >
-                                  <source src= {media.name} type="video/mp4"/>
-
-                        </video >
-                      )
-                        ))
-                  }
-                        
-                            </Carousel>):
-                            (
-                               <div
+                          }}
+                        >
+                          {allMedia.map((media) =>
+                            media.type === "image" ? (
+                              <div
                                 class="post-thumb thumb"
-                                style={{ mxaHeight: "500px" }} >
+                                style={{
+                                  mxaHeight: "400px",
+                                  width: "100%",
+                                  overflow: "hidden",
+                                }}
+                              >
                                 <img
-                                  src={`${address()}news/${news.id}/image`}
-                                  className="img-fullwidth img-responsive"
+                                  src={`${address()}news/${media.name}/image`}
+                                  // className="img-fullwidth img-responsive"
                                   alt=""
-                                  style = {{height:'400px'}}
+                                  style={{
+                                    height: "400px",
+                                    width: "100%",
+                                    overflow: "hidden",
+                                  }}
                                 />
                               </div>
+                            ) : (
+                              <div className="post-thumb thumb">
+                                <video
+                                  style={{
+                                    mxaHeight: "400px",
+                                    width: "100%",
+                                    overflow: "hidden",
+                                  }}
+                                  controls
+                                >
+                                  <source src={media.name} type="video/mp4" />
+                                </video>
+                              </div>
                             )
-                      } 
-                    
-
-                    </div>
-                    </article>
-                    </div>
-                    </div>
-                    <div class="col-md-6">
-
-                    <div class="entry-content">
-                      <div class="entry-meta media no-bg no-border mt-15 pb-20">
-                        <div class="media-body pl-15">
-                          <div class="event-content pull-left flip">
-                            <h2 class="line-bottom mt-0">{news.name}</h2>
-
-                            <h4 className="mt-0 mb-0 text-theme-colored">
-                              {news.startDate}
-                            </h4>
-                          </div>
+                          )}
+                        </Carousel>
+                      ) : (
+                        <div
+                          class="post-thumb thumb"
+                          style={{ mxaHeight: "400px" }}
+                        >
+                          <img
+                            src={`${address()}news/${news.id}/image`}
+                            // className="img-fullwidth img-responsive"
+                            alt=""
+                            style={{ height: "400px" }}
+                          />
                         </div>
+                      )}
+                    </div>
+                  </article>
+                </div>
+              </div>
+              <div class="col-md-5">
+                <div class="entry-content">
+                  <div class="entry-meta media no-bg no-border mt-15 pb-20">
+                    <div class="media-body pl-15">
+                      <div class="event-content pull-left flip">
+                        <h2 class="line-bottom mt-0">{news.name}</h2>
+
+                        <h4 className="mt-0 mb-0 text-theme-colored">
+                          {news.startDate}
+                        </h4>
                       </div>
+                    </div>
+                  </div>
 
-                      <p className="mb-15">{news.description}.</p>
+                  <p className="mb-15">{news.description}.</p>
 
-                        {/* <SocialMedia /> */}
-                   
+                  {/* <SocialMedia /> */}
                 </div>
               </div>
             </div>
