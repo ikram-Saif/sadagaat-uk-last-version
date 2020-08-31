a4ac6aaf848a9a136302d86d4c65c7a53e8d26e0
InasMahgoub <37179790+InasMahgoub@users.noreply.github.com>
2020-08-31 12:48:48 +0200

    Ongoing Projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import Header from '../sub_page_header';
-import { useTranslation } from 'react-i18next';
-import Project from '.'
+import React from "react";
+import Header from "../sub_page_header";
+import { useTranslation } from "react-i18next";
+import Project from ".";
 
+const Projects_ = () => {
+  const { t } = useTranslation();
 
-const Projects_=()=>{
-  
-   const {t} = useTranslation()
-  
-return(
-  <React.Fragment>
-    <section>
-      <Header name={t('Ongoing Projects')} coverImage = {'projects-bg-img'}/>
-      <Project type = {'ongoing'} />
-    </section>
-</React.Fragment>
-
-)
-
-}
+  return (
+    <React.Fragment>
+      <section>
+        <Header name={t("Ongoing Projects")} coverImage={"projects-bg-img"} />
+        <Project type={"ongoing"} />
+      </section>
+    </React.Fragment>
+  );
+};
 
 export default Projects_;
